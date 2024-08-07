2046b402aab828dd396d42b3161e7e9c80ad408f
Siddharth Patel <Sidd997701@gmail.com>
2024-08-07 22:06:19 +0530

    Create Test.c

---
+++ Chapter 1/Test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() 
+{
+    int i,j,A;
+    printf("Enter an integers:\n");
+    scanf("%d\n%d", &i, &j);
+    A = i+j;
+    printf("You entered:%d\n",A);
+
+    return 0;
+}
